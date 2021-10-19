4f84d60d63064e1a1686560747e43b60fa7d348b
Fahim Rayhan <fahimrayhan786@gmail.com>
2021-10-20 03:46:33 +0600

    more demo cards

--- scripts/main.js
+++ scripts/main.js
@@ -31,6 +31,34 @@ const Companies = [
         }
 
     },
+    {
+        name: "Apple",
+        logo: 'assets/Your Logo Here - 320x100.png',
+        type: "Company",
+        founded: 2021,
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        links: {
+            web: 'https://www.apple.com/',
+            fb: 'https://www.facebook.com/apple',
+            ld: 'https://www.linkedin.com/apple',
+            tw: 'https://www.twitter.com/apple',
+        }
+
+    },
+    {
+        name: "Apple",
+        logo: 'assets/Your Logo Here - 320x100.png',
+        type: "Company",
+        founded: 2021,
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        links: {
+            web: 'https://www.apple.com/',
+            fb: 'https://www.facebook.com/apple',
+            ld: 'https://www.linkedin.com/apple',
+            tw: 'https://www.twitter.com/apple',
+        }
+
+    },
 
 ];
 
